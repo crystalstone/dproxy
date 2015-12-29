6a1321c6834d0dddc5252fb4fc2f7c3145383fa4
crystalstone <wangshiying@zufangit.cn>
2015-12-29 20:18:15 +0800

    modify

--- src/index.js
+++ src/index.js
@@ -11,20 +11,20 @@ if (argv.h) {
 
     console.log(' 1. 全局安装');
     console.log(' 2. 设置全局代理 127.0.0.1， 端口号 8090(或其他); ');
-    console.log(' 3. dproxy -p 8090(端口号需要与第2部设置的代理一致), 这个命令来启动代理，' +
+    console.log(' 3. dpac -p 8090(端口号需要与第2部设置的代理一致), 这个命令来启动代理，' +
         '不写-p 参数，则代理端口号为8090。');
-    console.log(' 4. 启动dproxy 的项目的根目录，添加dproxy-rules 文件，规则如例子');
+    console.log(' 4. 启动dpac 的项目的根目录，添加dpac-rules 文件，规则如例子');
     return;
 }
 
 // 读取替换规则文件
-if (require('fs').existsSync('./dproxy-rules.js')) {
-    var absPath = require('path').resolve(process.cwd(), './dproxy-rules.js');
+if (require('fs').existsSync('./dpac-rules.js')) {
+    var absPath = require('path').resolve(process.cwd(), './dpac-rules.js');
     context.rules = require(absPath).rules;
     proxy.listen(port, "127.0.0.1");
 }
 else {
-    console.log("不存在dproxy-rules.js文件！");
+    console.log("不存在dpac-rules.js文件！");
 }
 
 
